b6ec185d79a6c9a1a81b03aeed1d3ff6a6a1ac85
Michelle Rossouw <michelleros128@gmail.com>
2021-08-28 17:28:18 +1000

    Srcloc: Added estd src loc display

--- MAVProxy/modules/mavproxy_srcloc.py
+++ MAVProxy/modules/mavproxy_srcloc.py
@@ -11,10 +11,21 @@ from pymavlink import mavutil
 import sys, traceback
 import numpy as np
 
+'''
+<message id="49" name="GPS_GLOBAL_ORIGIN">
+      <description>Publishes the GPS co-ordinates of the vehicle local origin (0,0,0) position. Emitted whenever a new GPS-Local position mapping is requested or set - e.g. following SET_GPS_GLOBAL_ORIGIN message.</description>
+      <field type="int32_t" name="latitude" units="degE7">Latitude (WGS84)</field>
+      <field type="int32_t" name="longitude" units="degE7">Longitude (WGS84)</field>
+      <field type="int32_t" name="altitude" units="mm">Altitude (MSL). Positive for up.</field>
+      <extensions/>
+      '''
+
 hlat = -353632621   #home location, in (approx) centimetres
 hlon = 1491652374
 slat = -353632261   #source location
 slon = 1491652321
+elat = 0 #estimated source location
+elon = 0 
     #[ab;bc] "is essentially 0.5 over the covariance matrix", A is the amplitude, and (x0, y0) is the center
 def gauss2d(x, y, amp, x0, y0, a, b, c, hlat, hlon):
     x = (x - hlat)*1e-3
@@ -43,10 +54,16 @@ class SrclocModule(mp_module.MPModule):
         'handle a MAVLink packet'''
         if m.get_type() == 'GLOBAL_POSITION_INT':
             #0.0000001 deg =~ 1 cm, i.e. m.lat & m.lon are approx in cm, thus divide by 1000 to get m
-            stre = gauss2d(m.lon, m.lat, 1, slon, slat, 0.25, 0, 0.5, hlat, hlon)
-            self.master.mav.plume_send(stre)
+            stre = gauss2d(m.lat, m.lon, 1, slat, slon, 0.25, 0, 0.5, hlat, hlon)
+            self.master.mav.plume_strength_send(stre)
             showIcon(self, 4, slat, slon, 'redstar.png')
-
+        if m.get_type() == 'PLUME_EST_LOC':
+            elat = hlat + m.x*111
+            elon = hlon + m.y*111
+            #print("Icon at: %.0f %.1f" % (elat, elon))
+            showIcon(self, 5, elat, elon, 'bluestar.png')
+#latitude means north
+#Latitude: -35.282001 
 def init(mpstate):
     '''initialise module'''
     return SrclocModule(mpstate)
