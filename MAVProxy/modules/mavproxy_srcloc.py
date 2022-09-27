19d16ea835cf319588150112e2d56ff04e9595be
Michelle Rossouw <michelleros128@gmail.com>
2022-09-27 12:19:49 +1000

    Srcloc: Removed Gaussian source estimation, cleaned up module & added ability to switch between Gaussian source and Pompy

--- MAVProxy/modules/mavproxy_srcloc.py
+++ MAVProxy/modules/mavproxy_srcloc.py
@@ -19,48 +19,25 @@ class SrclocModule(mp_module.MPModule):
     def __init__(self, mpstate):
         super(SrclocModule, self).__init__(mpstate, "srcloc", "srcloc module", multi_vehicle=True)
         '''initialisation code'''
+        self.LLMINV = 89.83204953368922 #lat lon to m inv
+        self.DEGTORAD = 3.141592653589793 / 180.0
         self.hlat = -353632621   #home location, in (approx) centimetres
         self.hlon = 1491652374
-        self.slat = self.hlat+200   #(only used for Gaussian) North-South #source location (currently just an offset from home for start point)
-        self.slon = self.hlon+100  #East-West
-        self.elat = 0 #estimated source location
-        self.elon = 0
-        self.upto = 0
-        self.done10 = 0
-        self.numsave = 200
-        self.dosl = 0
-        self.orst = True
-        self.xyarr = np.ones((2, self.numsave))*20e7 #for now, initialising to 20deg lat long & 0 strength - i.e. very far from the reading
-        self.strearr = np.zeros(self.numsave)
-        self.now = 0
-        self.prev = 0
-        self.gauEPar = np.array([0.5,0,0.5])
+        self.orst = True #to set origin on first message
         self.gauTPar = np.array([0.25, 0, 0.5])
-        self.cov = 0
-        self.stre = 0
-        self.PLUS = -1
         self.add_command('sl', self.cmd_sl, "Set source location", ['sl x y'])#['<%s|all>' % x])
         self.add_command('slp', self.cmd_load_pompy, "Load pompy data by number", ['slp no'])#['<%s|all>' % x])
-        # self.console.set_status('PlSt', '', row=6)
-        # self.showIcon('sl5', 0, 0, 'bluestar.png')
-        # self.console.set_status('PlEL', '', row=6)
-        # self.showIcon('sl4', 0, 0, 'redstar.png')
-        #self.console.set_status('PlTL', '', row=6)
-        #self.console.set_status('PlUs', '', row=6)
-        self.pompyuse = 0
+        self.pompyuse = 0 # 0 means use Gaussian
+        self.maxstr = 1
         self.pompy = np.flipud(np.loadtxt('/home/miche/pompy/ppo/blank.csv', delimiter=',', dtype="float32").T)
-        print("No Pompy data loaded.")
+        print("No Pompy data loaded. Using gaussian.")
         self.datasx = 1000
         self.datasy = 1000
         self.offx = 100
         self.offy = 500
         self.cenx = 875
         self.ceny = 500
-        self.maxstr = 1
-        self.LLMINV = 89.83204953368922 #lat lon to m inv
-        self.DEGTORAD = 3.141592653589793 / 180.0
-        plat, plon = self.toll((self.cenx-self.offx)/100,(self.ceny-self.offy)/100)
-        self.showIcon('sl5', plat, plon, 'bluestar.png')
+        self.slat, self.slon = self.toll((self.cenx-self.offx)/100,(self.ceny-self.offy)/100)
 
     #[ab;bc] "is essentially 0.5 over the covariance matrix", A is the amplitude, and (x0, y0) is the center
     def gauss2d(self, xy, amp, x0, y0, a, b, c):
@@ -99,6 +76,13 @@ class SrclocModule(mp_module.MPModule):
                             icon, layer=3, rotation=0, follow=False,
                             trail=mp_slipmap.SlipTrail(colour=(0, 255, 255))))
 
+    def changeHome(self, lat, lon):
+        self.hlat = lat
+        self.hlon = lon
+        self.orst = False #just to make sure GPS_GLOBAL_ORIGIN doesn't change it if it is manually set
+        self.slat, self.slon = self.toll((self.cenx-self.offx)/100,(self.ceny-self.offy)/100)
+        print("SL: Home/origin lat lon set to: %.0f %.0f, source lat lon set to %.0f %.0f" % (self.hlat, self.hlon, self.slat, self.slon))
+
     def toll(self, x, y): #m to lat, long
         dlat = float(x)*self.LLMINV
         scl = self.lonscl(self.hlat) #just using hlat as it's complex to do avg of both. No issue for small distances
@@ -122,18 +106,10 @@ class SrclocModule(mp_module.MPModule):
         'handle a MAVLink packet'''
         if m.get_type() == 'GPS_GLOBAL_ORIGIN' and self.orst:
             # message is sent when origin is initially set and when arming, apparently. Unfortunately no apparent way of triggering it.
-            self.hlat = m.latitude
-            self.hlon = m.longitude
-            print("SL: Origin lat lon set to: %.0f %.0f" % (self.hlat, self.hlon))
-            self.orst = False
+            self.changeHome(m.latitude, m.longitude)
         if m.get_type() == 'NAMED_VALUE_FLOAT':
             if m.name == 'GBEST':
                 self.console.set_status('gbest%d' % m.get_srcSystem(), 'GBest acc-to %d is %0.0f' % (m.get_srcSystem(), m.value), row=6)
-        #     if m.name == "PLUS":
-        #         self.PLUS = m.value
-        # if m.get_type() == 'DEBUG_VECT':
-        #     if m.name == 'PBEST':
-        #         self.console.set_status('pbest%d%d' % (m.get_srcSystem(),int(m.z)), 'PBest acc-to %0.0f for %d is %0.5f %0.5f ' % (m.get_srcSystem(), int(m.z), m.x, m.y), row=(7+int(m.z)))
         if m.get_type() == 'DEBUG_LOC':
             if re.match(r'^PBEST', m.name):
                 id = int(m.name[5:])
@@ -142,106 +118,51 @@ class SrclocModule(mp_module.MPModule):
                 self.console.set_status('pbest%d%d' % (m.get_srcSystem(),id), 'PBest acc-to %0.0f for %d is %d %d ' % (m.get_srcSystem(), id, m.lat, m.lon), row=(7+id))
                 self.showIcon('pbestst%d%d' % (m.get_srcSystem(),id), m.lat, m.lon, 'redstar.png')
         if m.get_type() == 'GLOBAL_POSITION_INT':
-            #0.0000001 deg =~ 1 cm, i.e. m.lat & m.lon are approx in cm, thus divide by 100 to get m
-            self.now = m.time_boot_ms
+            #0.0000001 deg =~ 1 cm, i.e. m.lat & m.lon are approx in cm
             sysid = m.get_srcSystem()
-            # cx, cy = self.tom(m.lat,m.lon)
-            # if abs(cx-((self.cenx-self.offx)/100))<1.0 and abs(cy-((self.ceny-self.offy)/100))<1.0:
-            #     print("Within 1m of source.")
-            self.stre = self.gauss2d((m.lat, m.lon), 1, self.slat, self.slon, self.gauTPar[0], self.gauTPar[1], self.gauTPar[2])
-            # self.stre = self.pompy2d(m.lat, m.lon)
-            self.master.mav.plume_strength_send(sysid, self.stre/self.maxstr)
-            self.console.set_status('sysid%d' % sysid, 'PLUS %0.7f sysid %d ' % (self.stre/self.maxstr, sysid), row=7)
-            if (self.now - self.prev) > 0.7e3:
-                #print("Running", self.now - self.prev)
-                self.xyarr[:,self.upto] = m.lat, m.lon
-                self.strearr[self.upto] = self.stre
-                self.upto = self.upto + 1
-                if self.upto > 19 & self.done10 == 0:
-                    self.done10 = 1
-                    print("Ready for SrcLoc Est")
-                if self.upto > (self.numsave-1):
-                    self.upto = 0
-                self.prev = self.now
-            if self.done10 == 1 & self.dosl == 1: #This only runs if target loc hasn't been sent
-                i = self.strearr.argmax()
-                guess = [1, self.xyarr[0,i], self.xyarr[1,i], self.gauEPar[0],self.gauEPar[1], self.gauEPar[2]]
-                pred_params, uncert_cov = opt.curve_fit(self.gauss2d, self.xyarr, self.strearr, p0=guess)
-                self.elat = pred_params[1]
-                self.elon = pred_params[2]
-                self.cov = np.mean(abs(uncert_cov))
-                #print("Sending plume loc: %.2f %.2f %.2f %.2f" % (self.elat, self.elon, m.alt, self.cov))
-                self.master.mav.plume_est_loc_send(int(self.elat), int(self.elon), int(m.alt), self.cov)
-                # self.master.mav.plume_par_send(self.gauEPar[0], self.gauEPar[1], self.gauEPar[2], self.gauTPar[0], self.gauTPar[1], self.gauTPar[2]) #removed this mavlink message
-        if m.get_type() == 'PLUME_EST_LOC':
-            #for use with onboard source estimation
-            self.elat, self.elon = self.toll(m.x,m.y)
-            self.showIcon('sl5', self.elat, self.elon, 'bluestar.png')
-            self.console.set_status('PlEL', 'PlEL %.7f %.7f cov %.4f' % (self.elat*1e-7, self.elon*1e-7, self.cov), row=6)
+            if self.pompyuse == 0: stre = self.gauss2d((m.lat, m.lon), 1, self.slat, self.slon, self.gauTPar[0], self.gauTPar[1], self.gauTPar[2])
+            else: stre = self.pompy2d(m.lat, m.lon)
+            self.master.mav.plume_strength_send(sysid, stre/self.maxstr)
+            self.console.set_status('sysid%d' % sysid, 'PLUS %0.7f sysid %d ' % (stre/self.maxstr, sysid), row=7)
 
     def cmd_sl(self, args):
         '''handle Source Location setting'''
         if len(args) == 0:
-             print("Usage: set x y | tpar a b c | epar a b c | ppar | setp lat lon | flyto on/off | dosrcloc on/off")
+             print("Usage: set x y | sethome lat lon | tpar a b c | ppar | flyto")
         else:
             if args[0] == 'set':
-                if len(args) == 3:
-                    self.slat, self.slon = self.toll(args[1],args[2])
-                else: 
-                    latlon = self.mpstate.click_location
-                    if latlon is not None:
-                        self.slat = int(latlon[0]*1.0e7)
-                        self.slon = int(latlon[1]*1.0e7)
-                    else:
-                        print('Click on map before running command to set location')
-                self.xyarr = np.ones((2, self.numsave))*20e7
-                self.strearr = np.zeros(self.numsave)
-                self.upto = 0
-                print('Set gauss source location to %.7f %.7f' % (self.slat*1e-7, self.slon*1e-7))
+                if self.pompyuse != 0:
+                    if len(args) == 3:
+                        self.slat, self.slon = self.toll(args[1],args[2])
+                    else: 
+                        latlon = self.mpstate.click_location
+                        if latlon is not None:
+                            self.slat = int(latlon[0]*1.0e7)
+                            self.slon = int(latlon[1]*1.0e7)
+                        else:
+                            print('Click on map before running command to set location')
+                    print('Set gauss source location to %.7f %.7f.' % (self.slat*1e-7, self.slon*1e-7))
+                else: print('Moving source only possible with Gaussian. For Pompy, must move home instead.')
             if args[0] == 'sethome':
                 if len(args) == 3:
-                    self.hlat = int(float(args[1])*1.0e7)
-                    self.hlon = int(float(args[2])*1.0e7)
+                    lat = int(float(args[1])*1.0e7)
+                    lon = int(float(args[2])*1.0e7)
+                    self.changeHome(lat, lon)
                 else:
                     latlon = self.mpstate.click_location
                     if latlon is not None:
-                        self.hlat = int(latlon[0]*1.0e7)
-                        self.hlon = int(latlon[1]*1.0e7)
+                        lat = int(latlon[0]*1.0e7)
+                        lon = int(latlon[1]*1.0e7)
+                        self.changeHome(lat, lon)
                     else:
                         print('Click on map before running command to set location')
-                print('Home is at %.7f %.7f' % (self.hlat*1e-7, self.hlon*1e-7))
-                plat, plon = self.toll((self.cenx-self.offx)/100,(self.ceny-self.offy)/100)
-                self.showIcon('sl5', plat, plon, 'bluestar.png')
             if args[0] == 'tpar':
                 self.gauTPar[0] = float(args[1])
                 self.gauTPar[1] = float(args[2])
                 self.gauTPar[2] = float(args[3])
                 print('Set source true params to %.2f %.2f %0.2f' % tuple(self.gauTPar))
-            if args[0] == 'epar':
-                self.gauEPar[0] = float(args[1])
-                self.gauEPar[1] = float(args[2])
-                self.gauEPar[2] = float(args[3])
-                print('Set source estimated params to %.2f %.2f %0.2f' % tuple(self.gauEPar))
             if args[0] == 'ppar':
-                print('Source estimated params are %.2f %.2f %0.2f' % tuple(self.gauEPar))
                 print('Source true params are %.2f %.2f %0.2f' %  tuple(self.gauTPar))
-            if args[0] == 'setp':
-                tlat, tlon = self.toll(args[1],args[2])
-                self.dosl = 0
-                print("Sending target loc: %.2f %.2f" % (tlat, tlon))
-                self.master.mav.plume_est_loc_send(int(tlat), int(tlon), 700, 0.999)
-                self.showIcon('sl5', tlat, tlon, 'bluestar.png')
-            if args[0] == 'setmult':
-                    self.mult = float(args[1])
-            if args[0] == 'dosrcloc': #fly to estimated source position
-                if args[1] == 'on':
-                    self.dosl = 1
-                if args[1] == 'off':
-                    self.dosl = 0
-                    self.showIcon('sl5', 0, 0, 'bluestar.png')
-                    self.console.set_status('PlEL', '', row=6)
-                    self.showIcon('sl4', 0, 0, 'redstar.png')
-                    self.console.set_status('PlTL', '', row=6)
     def cmd_load_pompy(self,args):
         if len(args) > 0:
             self.pompyuse = int(args[0])
@@ -252,12 +173,15 @@ class SrclocModule(mp_module.MPModule):
                 self.offx = int(args[2])
             else: 
                 self.offx = int(args[1])
-            plat, plon = self.toll((self.cenx-self.offx)/100,(self.ceny-self.offy)/100)
-            self.showIcon('sl5', plat, plon, 'bluestar.png')
+            self.slat, self.slon = self.toll((self.cenx-self.offx)/100,(self.ceny-self.offy)/100)
         else:
             print("No offset given. Using default")
         filegiven = True
-        if self.pompyuse == 1:
+        if self.pompyuse == 0:
+            print("Using Gaussian")
+            filegiven = False
+            self.maxstr = 1
+        elif self.pompyuse == 1:
             print("Loading ori but 1000x1000")
             filename = 'leng20_dt0.01_spdup5_wx2_wy0_px5_py0_sprd10_pfrel30_pfsp0.5_pfmax2000_pfmo20000000000.0_ng20_nd0.1_nb0.2_sx700_six1000_scx1000_ar1'
         elif self.pompyuse == 2:
@@ -297,18 +221,12 @@ class SrclocModule(mp_module.MPModule):
             self.pompy = np.flipud(np.loadtxt('/home/miche/pompy/ppo/'+filename+'.csv', delimiter=',', dtype="float32").T)
             self.maxstr = np.amax(self.pompy)
             print("Max strength is", self.maxstr)
+            self.slat, self.slon = self.toll((self.cenx-self.offx)/100,(self.ceny-self.offy)/100)
 
     def idle_task(self):
-    #     '''called on idle'''
-    #     # update status for detected plume strength
-        # self.console.set_status('PlSt', 'PS %.7f ut%d' % (self.stre/self.maxstr, self.upto), row=6)
-    #     # update icon & status for estimated location of source
-        if self.dosl == 1:
-            self.showIcon('sl5', self.elat, self.elon, 'bluestar.png')
-            self.console.set_status('PlEL', 'PlEL %.7f %.7f cov %.4f' % (self.elat*1e-7, self.elon*1e-7, self.cov), row=6)
-        #     # update icon & status for true location of source
-            self.showIcon('sl4', self.slat, self.slon, 'redstar.png')
-            self.console.set_status('PlTL', 'PlTL %.7f %.7f' % (self.slat*1e-7, self.slon*1e-7), row=6)
+        '''called on idle'''
+        #this is displayed from the idle task to ensure it is always updated to the current slat, slon variables.
+        self.showIcon('srcpos', self.slat, self.slon, 'bluestar.png')
 
 #latitude means north
 #Latitude: -35.282001
